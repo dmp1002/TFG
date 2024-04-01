3bc2ea558bc1ca8cf8dff28eb2160d4322fd85c3
dmp1002 <dmp1002@alu.ubu.es>
2024-04-01 11:46:31 +0200

    Método cargar_imagenes y carga de imagenes con OpenCV

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,28 @@
 import streamlit as st
+import cv2
 
-st.title('Cargar Imágenes')
+def cargar_imagenes():
+    st.title('Cargar Imágenes')
 
-archivo_cargado = st.file_uploader("Selecciona una imagen", type=["jpg", "jpeg", "png", "tiff", "tif"])
+    imagenes = []
 
-if archivo_cargado is not None:
-    st.image(archivo_cargado, caption='Imagen cargada.', use_column_width=True)
+    archivo_cargado = st.file_uploader("Selecciona una o más imágenes", type=["jpg", "jpeg", "png", "tiff", "tif"], accept_multiple_files=True)
+
+    if archivo_cargado is not None:
+        if isinstance(archivo_cargado, list):
+            for img in archivo_cargado:
+                st.image(img, width=200)
+                imagenes.append((img))
+        else:
+            st.image(archivo_cargado, width=200)
+            imagenes.append((archivo_cargado))
+
+    return imagenes
+
+imagenes=cargar_imagenes()
+
+imagen_DR = cv2.imread('./hojas/ESTEMPDR06072310549.tiff')
+imagen_WE = cv2.imread('./hojas/ESTEMPWE06072310549.tiff')
+
+st.image(imagen_DR, caption='Dry', width=150)
+st.image(imagen_WE, caption='Wet', width=150) 
