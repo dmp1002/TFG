5d6017c8e87f8bc0a3e67e35903d9f86bdbce924
dmp1002 <dmp1002@alu.ubu.es>
2024-04-01 13:01:35 +0200

    Mejora metodo cargar_imagenes y creacion metodo binarizar_imagenes

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import cv2
+import numpy as np
 
 def cargar_imagenes():
     st.title('Cargar Imágenes')
 
     imagenes = []
+    imagenes_grises =[]
 
     archivo_cargado = st.file_uploader("Selecciona una o más imágenes", type=["jpg", "jpeg", "png", "tiff", "tif"], accept_multiple_files=True)
 
@@ -13,16 +15,29 @@ def cargar_imagenes():
             for img in archivo_cargado:
                 st.image(img, width=200)
                 imagenes.append((img))
+                img_bytes = img.read()
+                img_gris = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_GRAYSCALE)
+                st.image(img_gris, width=200)
+                imagenes_grises.append((img_gris))
         else:
             st.image(archivo_cargado, width=200)
             imagenes.append((archivo_cargado))
+            img_bytes = archivo_cargado.read()
+            img_gris = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_GRAYSCALE)
+            img_gris=cv2.imread(archivo_cargado.img.uploaded_url)
+            st.image(img_gris, width=200)
+            imagenes_grises.append((img_gris))
 
-    return imagenes
+    return imagenes, imagenes_grises
 
-imagenes=cargar_imagenes()
+def binarizar_imagenes(imagenes_grises):
+    imagenes_binarias=[]
+    for img in imagenes_grises:
+        _, binary_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+        st.image(binary_img, width=200)
+        imagenes_binarias.append(binary_img)
 
-imagen_DR = cv2.imread('./hojas/ESTEMPDR06072310549.tiff')
-imagen_WE = cv2.imread('./hojas/ESTEMPWE06072310549.tiff')
+    return imagenes_binarias
 
-st.image(imagen_DR, caption='Dry', width=150)
-st.image(imagen_WE, caption='Wet', width=150) 
+imagenes, imagenes_grises=cargar_imagenes()
+imagenes_binarias=binarizar_imagenes(imagenes_grises)
